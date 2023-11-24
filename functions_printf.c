bf5209dfff5ebcb91e6e7fdc471960311bcf02ff
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-24 21:05:46 +0100

    update function_printf.c

--- functions_printf.c
+++ functions_printf.c
@@ -3,47 +3,48 @@
 #include <stdio.h>
 
 /**
- * print_char - Function to print a character.
- * @args: va_list containing the argument.
+ * print_char - Print a character.
+ * @args: A va_list containing the argument.
+ *
+ * Return: Number of characters printed (always 1).
  */
-int print_char(va_list *args)
+int print_char(va_list args)
 {
-	_putchar(va_arg(*args, int));
-	return (1);
-}
-/**
- * print_string - Function to print a string.
- * @args: va_list containing the argument.
- */
-int print_string(va_list *args)
-{
-    int i = 0;
-    int count = 0;
-    char *spec = va_arg(*args, char *);
-
-    if (spec == NULL)
-    {
-       spec = "(null)";
-    }
-
-    while (spec[i])
-    {
-        _putchar(spec[i]);
-        i++;
-        count++;
-    }
-
-    return (count);
+	return (_putchar(va_arg(args, int)));
 }
 
 /**
- * print_percent - Function to print a percent sign
- * @args: va_list containing the argument.
+ * print_string - Print a string.
+ * @args: A va_list containing the argument.
+ *
+ * Return: Number of characters printed.
  */
-int print_percent(__attribute__((unused))va_list *args)
-
+int print_string(va_list args)
 {
-	_putchar('%');
-	
-	return (1);
+	int count = 0;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	while (*str)
+	{
+		count += _putchar(*str);
+		str++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_percent - Print a percent sign.
+ * @args: A va_list containing the argument (unused).
+ *
+ * Return: Number of characters printed (always 1).
+ */
+int print_percent(__attribute__((unused)) va_list args)
+{
+	return (_putchar('%'));
 }
