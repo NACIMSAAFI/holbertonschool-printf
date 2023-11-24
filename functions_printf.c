154c62984d3e30e5411b2da445159055eb5050e7
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-24 11:16:34 +0100

    _printf.c

--- functions_printf.c
+++ functions_printf.c
@@ -17,18 +17,25 @@ int print_char(va_list *args)
  */
 int print_string(va_list *args)
 {
-	int i = 0;
-	int count = 0;
-	char *spec = va_arg(*args, char *);
-	if (spec == NULL)
-	{
-		return (-1);
-	}
-	for (i=0; spec[i] != '\0'; i++)
-		_putchar(spec[i]);
-	count ++;
-	return (count);
+    int i = 0;
+    int count = 0;
+    char *spec = va_arg(*args, char *);
+
+    if (spec == NULL)
+    {
+        return (-1);
+    }
+
+    while (spec[i])
+    {
+        _putchar(spec[i]);
+        i++;
+        count++;
+    }
+
+    return (count);
 }
+
 /**
  * print_percent - Function to print a percent sign
  * @args: va_list containing the argument.
