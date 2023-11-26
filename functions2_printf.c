001d633c522445e56338aa024a2e348bc3d6545b
NACIMSAAFI <7715@holbertonstudents.com>
2023-11-26 16:56:19 +0100

    Modification print-integer

--- functions2_printf.c
+++ functions2_printf.c
@@ -8,18 +8,10 @@ int print_decimal(va_list *args)
 {
     int count = 0;
 	int a = va_arg(*args, int);
-       
-    if (a == INT_MIN)
-    {
-        _putchar('-');
-        count++;
-      
-        a = -(INT_MIN + 1);
-    }
 	if (a < 0)
     {
 		_putchar('-');
-        a=-a;
+        a = -a;
         count ++;
     }
     count += print_all_number(a);
@@ -32,11 +24,18 @@ int print_decimal(va_list *args)
  */
 int print_integer(va_list *args)
 {
+    int count = 0;
 	int a = va_arg(*args, int);
 	if (a < 0)
+    {
 		_putchar('-');
-	return (print_all_number(a));
+        a = -a;
+        count ++;
+    }
+    count += print_all_number(a);
+	return (count);
 }
+
 
 /**
  * print_all_number - Function to print a number.
