4646eb7a3b6e2033647d53ca3fc9a67d99aa3b40
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-26 22:07:51 +0000

    update int MIN

--- functions2_printf.c
+++ functions2_printf.c
@@ -1,41 +1,32 @@
 #include "main.h"
 
 /**
- * print_decimal - Function to print a character.
+ * print_char - Function to print a character.
  * @args: va_list containing the argument.
  */
 int print_decimal(va_list *args)
 {
-    int count = 0;
 	int a = va_arg(*args, int);
+	if (a == INT_MIN)
+		return  (print_INT_MIN());
 	if (a < 0)
-    {
 		_putchar('-');
-        a = -a;
-        count ++;
-    }
-    count += print_all_number(a);
-	return (count);
+	return (print_all_number(a));
 }
 
 /**
- * print_integer - Function to print a character.
+ * print_char - Function to print a character.
  * @args: va_list containing the argument.
  */
 int print_integer(va_list *args)
 {
-    int count = 0;
 	int a = va_arg(*args, int);
+	if (a == INT_MIN)
+		return  (print_INT_MIN());
 	if (a < 0)
-    {
 		_putchar('-');
-        a = -a;
-        count ++;
-    }
-    count += print_all_number(a);
-	return (count);
+	return (print_all_number(a));
 }
-
 
 /**
  * print_all_number - Function to print a number.
@@ -45,18 +36,21 @@ int print_all_number(int a)
 {
 	int count = 0;
 	if (a < 0)
+	{
+		count += 1;
 		a = -a;
+	}
 
 	if (a == 0)
-    {
+	{
 		_putchar('0');
-        return(1);
-    }
+		return (1);
+	}
 
 	if (a / 10)
 		count += print_all_number(a / 10);
 
 	_putchar(a % 10 + '0');
-	count ++;
+	count += 1;
 	return (count);
 }
