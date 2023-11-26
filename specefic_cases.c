66a2687aa95d3abded5f0856da6cff006860161c
KhezamiTaha <khezamitaha10@gmail.com>
2023-11-26 22:05:16 +0000

    add cases file

---
+++ specefic_cases.c
@@ -0,0 +1,21 @@
+ #include "main.h"
+
+ /**
+ * print_INT_MIN - Function to print INT_MIN.
+ * @a: integer
+ */
+ int print_INT_MIN()
+ {
+	_putchar('-');
+	_putchar(2  +'0');
+	_putchar(1  +'0');
+	_putchar(4  +'0');
+	_putchar(7  +'0');
+	_putchar(4  +'0');
+	_putchar(8  +'0');
+	_putchar(3  +'0');
+	_putchar(6  +'0');
+	_putchar(4  +'0');
+	_putchar(8  +'0');
+	return (11);
+ }
